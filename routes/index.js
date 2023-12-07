4b10e9d75ddc9bb35d1edebfb62e78b93bc72613
Anirudha Jadhav <anirudhasj441@gmail.com>
2023-12-07 22:50:51 +0530

    update backend

--- routes/index.js
+++ routes/index.js
@@ -12,46 +12,11 @@ const hasChilds = async(dir) => {
     return childs.length > 0;
 }
 
-// const createNode = (dir) => {
-//     // var node = {}
-//     console.log("dir: ", dir)
-//     fs.readdir(dir, (err, files) => {
-//         files.forEach((file) => {
-//             let filePath = path.join(dir, file);
-//             let isDir = fs.lstatSync(filePath).isDirectory();
-//             console.log("isDir: ", isDir);
-//             let avatar = isDir ?
-//                 "../assets/editor-icons/default_folder.svg" :
-//                 "../assets/editor-icons/default_file.svg";
-//             var node = {
-//                 label: path.basename(file),
-//                 avatar: avatar,
-//                 selectable: !isDir,
-//                 renameActive: false,
-//                 newFolderActive: false,
-//                 newFileActive: false,
-//                 type: isDir ? 'folder' : 'file',
-//                 path: file,
-//             }
-//             console.log("node: ", node)
-//             if(!isDir) {
-//                 return node;
-//             }
-//             if(!hasChilds(file)){
-//                 return node;
-//             }
-//             node.children = createNode(filePath);
-//         })
-//     })
-//     // return node;
-// }
-
 const createNode = async(parent, dir) => {
     let dirPath = path.join(parent, dir);
     // console.log("dirPath: ", dirPath)
     let isDir = fs.lstatSync(dirPath).isDirectory();
     let avatar = isDir ? "folder" : "file";
-
 
     var node = {
         label: path.basename(dir),
@@ -64,13 +29,12 @@ const createNode = async(parent, dir) => {
         path: dirPath.split('/').splice(2).join('/')
         // path: dirPath
     }
-    // console.log(dirPath);
-    // console.log("isDir: ", isDir);
+
     if(!isDir) return node;
+
     let hasChild = await hasChilds(path.join(parent, dir));
-    // console.log('hasChild: ', hasChild);
     if(!hasChild) return node;
-    // if(!hasChilds(path.join(parent, dir))) return node;
+
     let childs = await fspromise.readdir(path.join(parent, dir));
 
     node.children = [];
@@ -78,6 +42,7 @@ const createNode = async(parent, dir) => {
         let childNode = await createNode(dirPath, childDir);
         node.children.push(childNode)
     }
+
     return node;
 }
 
@@ -90,23 +55,16 @@ router.get('/list_files', async(req, res) => {
 
     let dirs = await fspromise.readdir(rootDir, {recursive: false});
     var treeNodes = []
-    // await new Promise((resolve) => {
-    // })
     for(let dir of dirs){
         let node = await createNode(rootDir, dir);
-        treeNodes.push(node)
-        console.log("tree: ", treeNodes);
-        // console.log(node);
+        treeNodes.push(node);
     }
-    console.log("final tree: ", treeNodes)
-
-    // console.log(createNode(rootDir));
     res.send({tree: treeNodes});
 
 })
 
 router.post('/create_folder', (req, res) => {
-    data = req.body;
+    let data = req.body;
     console.log(data);
     let dirPath = path.join(rootDir, data.path);
     if(!fs.existsSync(dirPath)){
@@ -116,14 +74,37 @@ router.post('/create_folder', (req, res) => {
 })
 
 router.post('/create_file', (req, res) => {
-    data = req.body;
+    let data = req.body;
     let dirPath = path.join(rootDir, data.path);
     console.log(dirPath);
     console.log(fs.existsSync(dirPath))
     if(!fs.existsSync(dirPath)){
+        if(dirPath.includes('/')) {
+            let filename = path.basename(dirPath);
+            fs.mkdirSync(dirPath.replace(`/${filename}`, ''), {recursive: true});
+        }
         fs.closeSync(fs.openSync(dirPath, 'w'))
     }
     res.send({status: 200})
 })
 
+router.post('/rename_file', (req, res) => {
+    let data = req.body;
+    let oldPath = path.join(rootDir, data.old_path);
+    let newPath = path.join(rootDir, data.new_path);
+    if(!fs.existsSync(oldPath)) {
+        res.send({status: 200});
+        return;
+    }
+    fs.renameSync(oldPath, newPath);
+    res.send({staus: 200});
+})
+
+router.post('/get_file_content', (req, res) => {
+    let data = req.body;
+    let filePath = path.join(rootDir, data.path);
+    let fileContent = fs.readFileSync(filePath, 'utf-8');
+    // console.log(fileBuffer);
+    res.send({file_content: fileContent})
+})
 module.exports = router;
